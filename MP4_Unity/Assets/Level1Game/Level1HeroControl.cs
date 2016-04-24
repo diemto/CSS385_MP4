7b77226202c78246213cb1d63e8ba3fa95dc59e4
Diem To <diemto92@gmail.com>
2016-04-24 00:58:04 -0700

    turtle can change frontDir

--- MP4_Unity/Assets/Level1Game/Level1HeroControl.cs
+++ MP4_Unity/Assets/Level1Game/Level1HeroControl.cs
@@ -5,19 +5,31 @@ public class Level1HeroControl : MonoBehaviour {
 
 	#region user control references
 	private float kHeroSpeed = 20f;
-	private float kHeroRotateSpeed = 90/2f; // 90-degrees in 2 seconds
+	private bool isMovingRight;
 	#endregion
 	// Use this for initialization
 	void Start () {
 		// initialize projectile spawning
-
+		isMovingRight = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		#region user movement control
 		transform.position += Input.GetAxis ("Vertical")  * transform.up * (kHeroSpeed * Time.smoothDeltaTime);
-		transform.Rotate(Vector3.forward, -1f * Input.GetAxis("Horizontal") * (kHeroRotateSpeed * Time.smoothDeltaTime));
+		transform.position += Input.GetAxis ("Horizontal")  * transform.right * (kHeroSpeed * Time.smoothDeltaTime);
+
+		if(Input.GetAxis("Horizontal") < 0 && isMovingRight) {
+			float newX = transform.localScale.x * -1;
+			transform.localScale = new Vector3(newX, transform.localScale.y, transform.localScale.z);
+			isMovingRight = false;
+		}
+
+		if(Input.GetAxis("Horizontal") > 0 && !isMovingRight) {
+			float newX = transform.localScale.x * -1;
+			transform.localScale = new Vector3(newX, transform.localScale.y, transform.localScale.z);
+			isMovingRight = true;
+		}
 		#endregion
 	}
 
